6767b78b490f796673e41512e25b9be560a1f6e7
Ayush Nayyar <ayushnayyar48@gmail.com>
2023-11-15 00:24:10 +0530

    add: update account logic

--- src/controllers/account/update_one.js
+++ src/controllers/account/update_one.js
@@ -1,5 +1,30 @@
+import Account from "../../models/account.js";
+import User from "../../models/user.js";
+
 const updateOne = async (req, res) => {
-  return {};
+  const { name, type, balance } = req.body;
+  const account = req.account;
+  try {
+    if (name) {
+      account.name = name;
+    }
+    if (type) {
+      account.type = type;
+    }
+    if (balance) {
+      account.balance = balance;
+    }
+    const result = await Account.findOneAndUpdate(
+      { _id: account._id },
+      account
+    );
+    return res.status(200).json({ message: "Account updated" });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ message: "Something went wrong, account could not be fetched" });
+  }
 };
 
 export default updateOne;
