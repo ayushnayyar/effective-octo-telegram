d228123ac01bf67ab52bcf4cc05f4c354c3aaa5f
Ayush Nayyar <ayushnayyar48@gmail.com>
2023-11-18 01:56:50 +0530

    add: update transaction logic

--- src/controllers/transaction/update_one.js
+++ src/controllers/transaction/update_one.js
@@ -1,5 +1,33 @@
+import Transaction from "../../models/transaction.js";
+
 const updateOne = async (req, res) => {
-  return {};
+  const { amount, description, type, category } = req.body;
+  const transaction = req.transaction;
+  try {
+    if (amount) {
+      transaction.amount = amount;
+    }
+    if (type) {
+      transaction.type = type;
+    }
+    if (description) {
+      transaction.description = description;
+    }
+    if (category) {
+      transaction.category = category;
+    }
+    // TODO: handle failure of update operation
+    const result = await Transaction.findOneAndUpdate(
+      { _id: transaction._id },
+      transaction
+    );
+    return res.status(200).json({ message: "Transaction updated" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Something went wrong, transaction could not be updated",
+    });
+  }
 };
 
 export default updateOne;
